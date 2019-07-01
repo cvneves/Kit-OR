a11e5f18c22ce409807d72c5e2ce459e902eafed
carlos <carloskic1@gmail.com>
2019-07-01 01:07:06 -0300

    a

--- CG/src/GC.h
+++ CG/src/GC.h
@@ -4,6 +4,14 @@
 #include "Data.h"
 #include <ilcplex/ilocplex.h>
 
-void solve(Data &data,  std::vector<std::vector<int>> &patterns);
+#define EPSILON 0.000001
+
+typedef struct node
+{
+    
+
+} Node;
+
+void solve(Data &data, std::vector<std::vector<int>> &patterns);
 
 #endif
--- CG/src/main.cpp
+++ CG/src/main.cpp
@@ -15,13 +15,110 @@ int main(int argc, char **argv)
     Data data;
     data.readData(argv[1]);
 
-    //Construçao do subproblema
+    //Construçao do mestre
+    IloEnv env;
+    IloModel master(env);
+    IloNumVarArray lambda(env, data.getNItems(), 0.0, IloInfinity);
+    IloRangeArray constraints(env);
 
+    IloExpr sum(env);
+    for (int i = 0; i < data.getNItems(); i++)
+    {
+        sum += lambda[i];
+        constraints.add(lambda[i] == 1);
+    }
+
+    master.add(constraints);
+    IloObjective masterObj(env);
+    masterObj = IloMinimize(env, sum);
+    master.add(masterObj);
+
+    IloCplex BPP(master);
+
+    IloNumArray pi(env);
+
+    try
+    {
+        BPP.solve();
+    }
+
+    catch (IloException &e)
+    {
+        std::cout << e;
+    }
+
+    //construçao do subproblema
+    IloModel subproblem(env);
+    IloBoolVarArray x(env, data.getNItems());
+    IloExpr somaItems(env);
+    for (int i = 0; i < data.getNItems(); i++)
+    {
+        somaItems += data.getItemWeight(i) * x[i];
+    }
+    subproblem.add(somaItems <= data.getBinCapacity());
+    IloObjective subObj(env);
+    subproblem.add(subObj);
+
+    while (1)
+    {
+        BPP.getDuals(pi, constraints);
+
+        IloExpr somaPricing(env);
+        for (int i = 0; i < data.getNItems(); i++)
+        {
+            somaPricing += pi[i] * x[i];
+        }
+        subObj.setExpr(1 - somaPricing);
+
+        IloCplex pricing(subproblem);
+
+        try
+        {
+            pricing.solve();
+        }
+
+        catch (IloException &e)
+        {
+            std::cout << e;
+        }
+
+        IloNumArray x_vals(env, data.getNItems());
+        pricing.getValues(x_vals, x);
+
+        if (pricing.getObjValue() < -EPSILON)
+        {
+            lambda.add(IloNumVar(masterObj(1) + constraints(x_vals), 0.0, IloInfinity));
+
+            try
+            {
+                BPP.solve();
+            }
+            catch (IloException &e)
+            {
+                std::cout << e;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    for (int i = 0; i < lambda.getSize(); i++)
+    {
+        std::cout << BPP.getValue(lambda[i]) << "\n";
+    }
+
+    std::cout << "Status: " << BPP.getStatus() << "\n";
+    std::cout << "Bins usados: " << BPP.getObjValue() << "\n\n\n\n\n";
+
+    BPP.exportModel("modelo.lp");
+
+    env.end();
 
     //Solve
-    std::vector<std::vector<int>> patterns;
-    solve(data, patterns);
-
+    // std::vector<std::vector<int>> patterns;
+    // solve(data, patterns);
 
     return 0;
 }
