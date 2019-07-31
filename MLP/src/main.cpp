f2741eae4141d233da0d3ad9be898fd0c4adf72f
carlos <carloskic1@gmail.com>
2019-07-30 23:08:14 -0300

    a

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -18,7 +18,6 @@ int N;      // quantidade total de vertices
 double calculaCustoAcumulado(std::vector<int> &s);
 double calculaCustoSubsequencia(std::vector<int> &s);
 
-
 int main(int argc, char **argv)
 {
   std::vector<std::pair<std::pair<int, int>, double>> custo_insercao;
@@ -31,29 +30,45 @@ int main(int argc, char **argv)
   printData();
 
   std::vector<int> s, s1, s2;
-  for(int i = 0; i < N; i++)
+  for (int i = 0; i < N; i++)
   {
-    s.push_back(i+1);
+    s.push_back(i + 1);
   }
-  s.push_back(1);
+  // s.push_back(1);
 
-  for(int i = 0; i < 15; i++)
+  for (int i = 0; i < 7; i++)
   {
-    s1.push_back(i+1);
+    s1.push_back(i + 1);
+    
   }
-  for(int i = 15; i < N; i++)
+  for (int i = 7; i < N; i++)
   {
-    s2.push_back(i+1);
+    s2.push_back(i + 1);
   }
-  s2.push_back(1);
+  // s2.push_back(1);
 
+  for(int i = 0; i < s1.size(); i++)
+  {
+    std::cout << s1[i] << " ";
+  }
   std::cout << "\n";
+
+  for(int i = 0; i < s2.size(); i++)
+  {
+    std::cout << s2[i] << " ";
+  }
+  std::cout << "\n";
+
+
   std::cout << calculaCustoAcumulado(s) << "\n";
 
-  std::cout << calculaCustoSubsequencia(s1) << "\n";
-  std::cout << calculaCustoSubsequencia(s2) << "\n";
+  double T1 = calculaCustoSubsequencia(s1);
+  double T2 = calculaCustoSubsequencia(s2);
 
+  double C1 = calculaCustoAcumulado(s1);
+  double C2 = calculaCustoAcumulado(s2);
 
+  std::cout << C1 + (s2.size()) * (T1 + M[s1[6]][s2[0]]) + C2;
 
   return 0;
 }
@@ -61,9 +76,9 @@ int main(int argc, char **argv)
 double calculaCustoSubsequencia(std::vector<int> &s)
 {
   double custo = 0;
-  for(int i = 0; i < s.size()-1; i++)
+  for (int i = 0; i < s.size()-1; i++)
   {
-    custo +=  M[s[i]][s[i+1]];
+    custo += M[s[i]][s[i + 1]];
   }
   return custo;
 }
@@ -71,12 +86,12 @@ double calculaCustoSubsequencia(std::vector<int> &s)
 double calculaCustoAcumulado(std::vector<int> &s)
 {
   double custo = 0;
-  for(int i = 1; i < s.size()-1; i++)
+  for (int i = 1; i < s.size(); i++)
   {
-    for(int j = 0; j < i; j++)
+    for (int j = 0; j < i; j++)
     {
-      std::cout << s[j] << " -> " << s[j+1] << "\n";
-      custo += M[s[j]][s[j+1]];
+      std::cout << s[j] << " -> " << s[j + 1] << "\n";
+      custo += M[s[j]][s[j + 1]];
     }
     std::cout << "\n";
   }
