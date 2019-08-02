4468673d3dd5c0830b123016317114dada8b5c29
a <a>
2019-08-02 19:28:25 -0300

    a

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -16,18 +16,23 @@ double **M; // matriz de adjacencia
 int N;      // quantidade total de vertices
 
 double calculaCustoAcumulado(std::vector<int> &s);
-double calculaCustoSubsequencia(std::vector<int> &s);
+double calculaCustoSubsequencia(std::vector<int> &s, int i, int j);
 
 void printSolution(std::vector<int> &s);
 
+void reinsertion(std::vector<int> &solucao, int i, int tamanho_subsequencia, int j);
+void swap(std::vector<int> &solucao, int i, int j);
+void twoOpt(std::vector<int> &solucao, int i, int j);
+
+void buscaVizinhancaSwap(std::vector<int> &s, std::vector<std::vector<std::vector<double>>> ReOpt, double &valor_obj);
+
 std::vector<int> construction(double alpha);
-std::vector<std::vector<std::vector<int>>> reOptPreProcessing(std::vector<int> &s);
+std::vector<std::vector<std::vector<double>>> reOptPreProcessing(std::vector<int> &s);
 
 int main(int argc, char **argv)
 {
   std::vector<std::pair<std::pair<int, int>, double>> custo_insercao;
   double alpha = 0.5;
-  double valor_obj;
 
   srand(time(NULL));
 
@@ -37,11 +42,18 @@ int main(int argc, char **argv)
   std::vector<int> s;
   s = construction(0.5);
 
+  std::cout << "\n";
+
   printSolution(s);
+  // std::cout << '\n'
+  //           << calculaCustoAcumulado(s) << '\n';
+  std::vector<std::vector<std::vector<double>>> reOpt = reOptPreProcessing(s);
+  double valor_obj = reOpt[0][0][1];
 
-  std::cout << calculaCustoAcumulado(s) << "\n";
+  // std::cout << valor_obj << '\n';
+  buscaVizinhancaSwap(s, reOpt, valor_obj);
 
-  reOptPreProcessing(s);
+  // std::cout << calculaCustoSubsequencia(s, 0, 1);
 
   return 0;
 }
@@ -80,10 +92,10 @@ std::vector<int> construction(double alpha)
   return s;
 }
 
-std::vector<std::vector<std::vector<int>>> reOptPreProcessing(std::vector<int> &s)
+std::vector<std::vector<std::vector<double>>> reOptPreProcessing(std::vector<int> &s)
 {
   // T = 0, C = 1, W = 2
-  std::vector<std::vector<std::vector<int>>> reOpt(3, std::vector<std::vector<int>>(N + 1, std::vector<int>(N + 1, 0)));
+  std::vector<std::vector<std::vector<double>>> reOpt(3, std::vector<std::vector<double>>(N + 1, std::vector<double>(N + 1, 0)));
 
   for (int i = 0; i < N; i++)
   {
@@ -101,13 +113,12 @@ std::vector<std::vector<std::vector<int>>> reOptPreProcessing(std::vector<int> &
 
   for (int t = 2; t <= N + 1; t++)
   {
-    std::cout << "\n";
     for (int i = 0; i < N - t + 2; i++)
     {
       reOpt[2][i][i + t - 1] = reOpt[2][i][i + t - 2] + reOpt[2][i + t - 1][i + t - 1];
-      reOpt[0][i][i + t - 1] = reOpt[0][i][i + t - 2] + M[s[i + t - 2]][s[i + t - 1]] + reOpt[0][i + t - 1][i + t - 1];
+      reOpt[0][i][i + t - 1] = reOpt[0][i][i + t - 2] + M[s[i + t - 2]][s[i + t - 1]];
       reOpt[1][i][i + t - 1] = reOpt[1][i][i + t - 2] + reOpt[2][i + t - 1][i + t - 1] * (reOpt[0][i][i + t - 2] + M[s[i + t - 2]][s[i + t - 1]]) + reOpt[1][i + t - 1][i + t - 1];
-      std::cout << reOpt[1][i][i + t - 1] << " ";
+      // std::cout << reOpt[0][i][i + t - 1] << " ";
 
       // for (int j = i; j < i + t; j++)
       // {
@@ -115,17 +126,18 @@ std::vector<std::vector<std::vector<int>>> reOptPreProcessing(std::vector<int> &
       // }
       // std::cout << "\n";
     }
+    // std::cout << "\n";
   }
 
   return reOpt;
 }
 
-double calculaCustoSubsequencia(std::vector<int> &s)
+double calculaCustoSubsequencia(std::vector<int> &s, int i, int j)
 {
   double custo = 0;
-  for (int i = 0; i < s.size() - 1; i++)
+  for (int u = i; u < j; u++)
   {
-    custo += M[s[i]][s[i + 1]];
+    custo += M[s[u]][s[u + 1]];
   }
   return custo;
 }
@@ -156,6 +168,55 @@ void printSolution(std::vector<int> &s)
     std::cout << s[i] << " ";
   }
   std::cout << "\n";
+}
+
+void twoOpt(std::vector<int> &solucao, int i, int j)
+{
+  std::reverse(solucao.begin() + i, solucao.begin() + j + 1);
+}
+
+void reinsertion(std::vector<int> &solucao, int i, int tamanho_subsequencia, int j)
+{
+  int vertex = solucao[i];
+
+  std::vector<int> subsequencia(solucao.begin() + i, solucao.begin() + i + tamanho_subsequencia);
+
+  solucao.erase(solucao.begin() + i, solucao.begin() + i + tamanho_subsequencia);
+  solucao.insert(solucao.begin() + j, subsequencia.begin(), subsequencia.end());
+}
+
+void swap(std::vector<int> &solucao, int i, int j)
+{
+  std::swap(solucao[i], solucao[j]);
+}
+
+void buscaVizinhancaSwap(std::vector<int> &s, std::vector<std::vector<std::vector<double>>> reOpt, double &valor_obj)
+{
+  double T, C, W, melhor_valor_obj = std::numeric_limits<double>::infinity(), temp_obj;
+  int melhor_i, melhor_j;
+  bool improved = false;
+  for (int i = 1; i < s.size() - 2; i++)
+  {
+    for (int j = i + 2; j < s.size() - 1; j++)
+    {
+      W = reOpt[2][0][i] + reOpt[2][j][j];
+      T = reOpt[0][0][i] + M[s[i]][s[j]] + reOpt[0][j][j];
+      // C = reOpt[1][0][i] + reOpt[2][0][i] * (reOpt[0][i][i] + M[i - 1][i]) + reOpt[1][i][i];
+
+      std::cout << i << " " << j << "\n";
+      // swap(s, i, j);
+      // std::cout << calculaCustoSubsequencia(s, 0, i) << "\n";
+      // swap(s, i, j);
+
+      std::cout << T << "\n";
+    }
+  }
+
+  // if (improved == true)
+  // {
+  //   valor_obj = melhor_valor_obj;
+  //   swap(s, melhor_i, melhor_j);
+  // }
 }
 
 void printData()
