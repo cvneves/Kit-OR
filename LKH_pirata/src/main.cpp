7f6973236d446ad41fdd2e3c218be42b2e42c7ff
a <a>
2019-08-26 18:28:23 -0300

    a

--- LKH_pirata/src/main.cpp
+++ LKH_pirata/src/main.cpp
@@ -21,26 +21,26 @@ using namespace std;
 int dimension;
 double **matrizAdj;
 
-typedef pair<int, double> ii;
+typedef pair<double, std::pair<int, int>> dii;
 typedef vector<vector<double>> vvi;
-typedef vector<ii> vii;
+typedef vector<dii> vii;
 typedef vector<int> vi;
 
-vi taken;              // global boolean flag to avoid cycle
-priority_queue<ii> pq; // priority queue to help choose shorter edges
-                       // note: default setting for C++ STL priority_queue is a max heap
+vi taken;               // global boolean flag to avoid cycle
+priority_queue<dii> pq; // priority queue to help choose shorter edges
+                        // note: default setting for C++ STL priority_queue is a max heap
 
 int V;
-std::vector<std::vector<ii>> AdjList;
+std::vector<std::vector<dii>> AdjList;
 
 void process(int vtx)
 { // so, we use -ve sign to reverse the sort order
   taken[vtx] = 1;
   for (int j = 0; j < (int)AdjList[vtx].size(); j++)
   {
-    ii v = AdjList[vtx][j];
-    if (!taken[v.first])
-      pq.push(ii(-v.second, -v.first));
+    dii v = AdjList[vtx][j];
+    if (!taken[v.second.first])
+      pq.push({-v.first, {-v.second.first, -v.second.second}});
   }
 } // sort by (inc) weight then by (inc) id
 
@@ -56,14 +56,14 @@ int main(int argc, char **argv)
 
   std::chrono::time_point<std::chrono::system_clock> start, end;
 
-  AdjList = std::vector<std::vector<ii>>(V, std::vector<ii>(V));
+  AdjList = std::vector<std::vector<dii>>(V, std::vector<dii>(V));
 
   for (int i = 0, c = 0; i < V; i++)
   {
     for (int j = 0; j < V; j++, c++)
     {
-      AdjList[i][j].second = matrizAdj[i + 1][j + 1];
-      AdjList[i][j].first = j;
+      AdjList[i][j].first = matrizAdj[i + 1][j + 1];
+      AdjList[i][j].second = {j, i};
 
       // std::cout << AdjList[i][j].first << " ";
     }
@@ -76,22 +76,21 @@ int main(int argc, char **argv)
   // inside int main()---assume the graph is stored in AdjList, pq is empty
   taken.assign(V, 0); // no vertex is taken at the beginning
   parent.assign(V, -1);
-  process(0);         // take vertex 0 and process all edges incident to vertex 0
+  process(0); // take vertex 0 and process all edges incident to vertex 0
   int previous = 0;
   double mst_cost = 0;
-  int u, w;
+  int u, v, w;
   while (!pq.empty())
   { // repeat until V vertices (E=V-1 edges) are taken
-    ii front = pq.top();
+    dii front = pq.top();
 
     pq.pop();
-    u = -front.second, w = -front.first; // negate the id and weight again
+    u = -front.second.first, v = -front.second.second, w = -front.first; // negate the id and weight again
 
     if (!taken[u])
     {                            // we have not connected this vertex yet
       mst_cost += w, process(u); // take u, process all edges incident to u
-      parent[u] = previous;
-      previous = u;
+      parent[u] = v;
     }
   } // each edge is in pq only once!
 
@@ -100,11 +99,15 @@ int main(int argc, char **argv)
   int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
   std::cout << "Tempo total (s): " << elapsed_seconds / 1000.0 << "\n\n";
   std::cout << mst_cost << "\n";
-  
-  // for(int i = 0; i < parent.size(); i++)
-  // {
-  //   std::cout << i << " " << parent[i] << "\n";
-  // }
+
+  double CUSTOTOTAL = 0;
+  for (int i = 0; i < parent.size(); i++)
+  {
+    CUSTOTOTAL += matrizAdj[i + 1][parent[i] + 1];
+    std::cout << i + 1 << " " << parent[i] + 1 << "\n";
+  }
+
+  std::cout << CUSTOTOTAL << "\n";
 
   return 0;
 }
